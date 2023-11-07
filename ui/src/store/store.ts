d82582f30a898800ff72f9c15b2cc2d2ad9c0b84
Arttu Ylärakkola <arttu@solvalou.com>
2023-11-07 14:03:18 +0200

    ongoing

--- ui/src/store/store.ts
+++ ui/src/store/store.ts
@@ -6,6 +6,8 @@ import layers from "./layersSlice";
 import repaint from "./repaintSlice";
 import tools from "./toolsSlice";
 
+import { repaint as repaintAction } from '../store/repaintSlice';
+
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { restoreStateImageMaskPixelAttributeData } from "../utils/utils";
 import repaintScreenMiddleware from "./repaintScreenMiddleware";
@@ -33,5 +35,7 @@ export type AppDispatch = typeof store.dispatch
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+
+setTimeout(() => store.dispatch(repaintAction()), 10);
 
 export default store;
