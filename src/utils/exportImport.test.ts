70a99a76fd947ad551396b889a017199162907f0
Arttu Ylärakkola <arttu@solvalou.com>
2023-11-18 14:47:48 +0200

    forgot to add file

---
+++ src/utils/exportImport.test.ts
@@ -0,0 +1,19 @@
+import { pack8bit } from "./exportImport";
+
+describe('export/import', () => {
+
+    test('pack 8 bit', () => {
+        expect(pack8bit([
+            128,
+            255,
+            0,
+            64,
+            23,
+            1,
+            2,
+            3,
+            4
+        ])).toBe('foo');
+    });
+
+});
